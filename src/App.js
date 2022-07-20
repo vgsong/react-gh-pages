95c2b85d5c5aedbbcd51471f09bc488e6217d25b
v son <vctrsong@gmail.com>
2022-07-20 10:28:38 -0700

    porra

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reactoooooor
         </a>
       </header>
     </div>
